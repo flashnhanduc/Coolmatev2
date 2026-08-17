a493bd87ebff5acd4a598087343e26b370391337
ducxinloi123 <152901655+ducxinloi123@users.noreply.github.com>
2026-08-17 18:02:58 +0700

    add product detail page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Shop\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/san-pham/{product:slug}',[ProductController::class, 'show'])->name('products.show');
